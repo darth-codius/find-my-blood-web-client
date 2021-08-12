f42e75c7cb62fb499911b91b318c1486fb462a99
jessydaniella <jessydaniella2014@yahoo.com>
2021-08-12 13:05:41 +0100

    i worked on signin page

---
+++ src/Components/Pages/Signin.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Signin() {
+    return (
+        <div>
+            <h1>LOGIN</h1>
+            <p>Welcome Back</p>
+  <form>
+        <label>
+    Email Address
+    <input type="text" Email Address="email" />
+    <br />
+      </label>
+
+     
+      <label>
+    Password
+   <input type="password" Password="password" />
+    <p>Forgot Password?</p>
+   <br />
+      </label>
+     
+  </form>
+  <button>LOGIN</button>
+
+  <p>Need an account?       Signup</p>
+        </div>
+    )
+}
