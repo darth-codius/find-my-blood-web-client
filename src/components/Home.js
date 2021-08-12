18b4c93973b307ed821633fd3dd88e695116c935
teewo0 <teewodavie@gmail.com>
2021-08-12 12:59:14 +0100

    landing page

---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <nav className="homenav">
+                <div className="nav1">
+                    <h6>GET BLOOD</h6>
+                </div>
+                <div className="buttonnav">
+                    
+                    <button>Sign In</button>
+                    <button>Sign Up</button>
+
+                </div>
+            </nav>
+            <div className="div2">
+                <div>
+                    <h1>Get blood, Save Lives</h1>
+                    <p>We are linked by blood, and blood is memory without language</p>
+                    <button>Get Started</button>
+                </div>
+                <div>
+                    icon
+                </div>
+            </div>
+        </div>
+    )
+}
