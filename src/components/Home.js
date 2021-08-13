2afb8979dd19154302e2a2b48f0addcd6ee9997f
teewo0 <teewodavie@gmail.com>
2021-08-13 13:11:55 +0100

    day2

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,19 +9,36 @@ export default function Home() {
                 </div>
                 <div className="buttonnav">
                     
-                    <button>Sign In</button>
-                    <button>Sign Up</button>
+                    <button type="button" class="btn btn-outline-danger">Login</button>
+                    <button type="button" class="btn btn-outline-danger">Register</button>
 
                 </div>
             </nav>
-            <div className="div2">
+            
+            <div class="card mb-3" className="card1">
+              <div class="row g-0">
+                <div class="col-md-6">
+                  <div class="card-body" className="card2">
+                    <h1 class="card-title" className="cardtittle">Get blood, Save Lives</h1>
+                    <p class="card-text" className="cardtext">We are linked by blood, and blood is memory without language</p>
+                    <button type="button" className="button3">Get Started</button>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                   <img src="../../../img/Laboratory-pana.svg" class="img-fluid rounded-start" alt="..."/>
+                </div>
+              </div>
+            </div>
+            
+            <div>
                 <div>
-                    <h1>Get blood, Save Lives</h1>
-                    <p>We are linked by blood, and blood is memory without language</p>
-                    <button>Get Started</button>
+                    <img src="..." alt="..."/>
                 </div>
                 <div>
-                    icon
+                    <h1>Download our mobile app</h1>
+                    <hr/>
+                    <p>Our dedicated hospital engagement app and web portal allow you to access information instantaneously (no tedious form, long calls, or administrative hassle) and securely</p>
+                    <button type="button" className="button4">Download App</button>
                 </div>
             </div>
         </div>
