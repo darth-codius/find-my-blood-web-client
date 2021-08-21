66517bff9d425d4df77f0dab9f629fcf7ecb0d0b
teewo0 <teewodavie@gmail.com>
2021-08-21 11:46:04 +0100

    Merge branch 'main' of https://github.com/teewo0/find-my-blood-web-client into main

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import "./App.css";
-import Home from "./components/Home";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import RequestPage from "./components/RequestPage";
-import SentRequests from "./components/SentRequests";
-import Profile from "./components/Profile";
+import Home from "./Components/Home";
+import NavBar from "./Components/NavBar";
+import Footer from "./Components/Footer";
+import RequestPage from "./Components/RequestPage";
+import SentRequests from "./Components/SentRequests";
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Signin from "./Components/Signin";
+import Landing from "./Components/Landing";
+import Signup from "./Components/Pages/Success";
+import Request from "./Components/Pages/Request";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Home /> */}
-      {/* <NavBar /> */}
-      {/* <Footer/> */}
-      {/* <RequestPage/> */}
-      {/* <SentRequests/> */}
-      <Profile/>
-
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/landing" exact>
+          <Landing />
+        </Route>
+        <Route path="/login" exact>
+          <Signin />
+        </Route>
+        <Route path="/signup" exact>
+          <Signup />
+        </Route>
+        <Route path="/request" exact>
+          <Request />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export default function Login() {
+    return (
+            
+
+            <div className="input">
+                <input type="text" placeholder="Username"/>
+                <input type="text" placeholder="Password"/>
+                <button>Login</button>
+                <p>Please Login with your username and Password</p>
+
+            </div>
+        
+    )
+}
