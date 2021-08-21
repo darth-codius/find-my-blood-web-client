1786075119e7df0a7cea3ad82ca32004603db140
teewo0 <teewodavie@gmail.com>
2021-08-21 13:04:56 +0100

    worked on profile

--- src/App.js
+++ src/App.js
@@ -9,30 +9,33 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Signin from "./Components/Signin";
 import Landing from "./Components/Landing";
-import Signup from "./Components/Pages/Success";
-import Request from "./Components/Pages/Request";
+import Signup from "./Components/Success";
+import Request from "./Components/Request";
+import Profile from "./Components/Profile";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/landing" exact>
-          <Landing />
-        </Route>
-        <Route path="/login" exact>
-          <Signin />
-        </Route>
-        <Route path="/signup" exact>
-          <Signup />
-        </Route>
-        <Route path="/request" exact>
-          <Request />
-        </Route>
-      </Switch>
-    </Router>
+    // <Router>
+    //   <Switch>
+    //     <Route path="/" exact>
+    //       <Home />
+    //     </Route>
+    //     <Route path="/landing" exact>
+    //       <Landing />
+    //     </Route>
+    //     <Route path="/login" exact>
+    //       <Signin />
+    //     </Route>
+    //     <Route path="/signup" exact>
+    //       <Signup />
+    //     </Route>
+    //     <Route path="/request" exact>
+    //       <Request />
+    //     </Route>
+    //   </Switch>
+    // </Router>
+
+    <Profile/>
   );
 }
 
--- src/Components/Success.js
+++ src/Components/Success.js
@@ -1,27 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Error.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Success.css";
 
 export default function Success() {
   return (
     <div>
- 
-          <div className="logo4">
+      <div className="logo4">
         <img src="../../../img/vaccine.svg" alt="" />
-
-          </div>
-          <h1>Request Successful!</h1>
-          <p>Your request was successful </p>
-          <Link to='/'></Link>
-          <button class="btn3">Go Back Home</button>
-            {/* <img src="../../../img/error.svg" alt="" />
+      </div>
+      <h1 className="errortext">Request Successful!</h1>
+      <p>Your request was successful </p>
+      <Link to="/"></Link>
+      <button class="btn3">Go Back Home</button>
+      {/* <img src="../../../img/error.svg" alt="" />
             </div>
             <h1>Oops! this page is unknown or does not exist </h1>
           <p>Sorry about that, but the page you are looking for is'nt available</p>
           {/* <Link to='/'></Link> */}
-       
-        </div>
     </div>
-  )
+  );
 }
-
